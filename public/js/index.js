5bb13b7f364e61e0e807b126522cd05579db224b
marta <martysuk99@gmail.com>
2018-11-17 17:15:16 +0200

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -9,11 +9,18 @@ socket.on('disconnect', () => {
 })
 
 socket.on('newMessage', (message) => {
-    //console.log('New message', message)
     let formattedTime = moment(message.createdAt).format('h:mm a')
-    let li = jQuery('<li></li>')
-    li.text(`${message.from} ${formattedTime}: ${message.text}`)
-    jQuery('#messages').append(li)
+    let template = jQuery('#message-template').html()
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    })
+    jQuery('#messages').append(html)
+
+    // let li = jQuery('<li></li>')
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`)
+    // jQuery('#messages').append(li)
 })
 
 // socket.emit('createMessage', {
@@ -25,14 +32,23 @@ socket.on('newMessage', (message) => {
 
 
 socket.on('newLocationMessage', (message) => {
-    let li = jQuery('<li></li>')
-    let a = jQuery('<a target="_blank">My current location</a>')
-    let formattedTime = moment(message.createdAt).format('h:mm a')
 
-    li.text(`${message.from} ${formattedTime}: `)
-    a.attr('href', message.url)
-    li.append(a)
-    jQuery('#messages').append(li)
+    let formattedTime = moment(message.createdAt).format('h:mm a')
+    let template = jQuery('#location-message-template').html()
+    let html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    })
+    jQuery('#messages').append(html)
+
+
+    // let li = jQuery('<li></li>')
+    // let a = jQuery('<a target="_blank">My current location</a>')
+    // li.text(`${message.from} ${formattedTime}: `)
+    // a.attr('href', message.url)
+    // li.append(a)
+    // jQuery('#messages').append(li)
 })
 
 jQuery('#message-form').on('submit', (e) => {
